2c61935a5d85ffbf7d85bd20dd9ada450c020a2b
sevkan <sevkan2009@hotmail.com>
2026-02-03 15:24:53 +0300

    3.0.3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {} // Bu satır "process is not defined" hatasını kökten çözer
+    'process.env': {}
   },
   build: {
     outDir: 'build'
